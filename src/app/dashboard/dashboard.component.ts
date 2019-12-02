cd6652a15c60cc701a26d46785b52caa34bf9c28
ipiongg <ipiongg@gmail.com>
2019-12-02 08:09:22 -0300

    função de retorno de usuarios cadastrados e função de retorno quantidade de analises realizadas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ɵConsole } from '@angular/core';
 import { DataService } from '../data.service';
 import { map } from 'rxjs/operators';
 import { Chart } from 'chart.js';
@@ -16,8 +16,8 @@ export class DashboardComponent implements OnInit {
   quantidade = 50;
   qtAnalisetexto = null;
   qtAnalisecoment = null;
-  lista2= [35];
-  lista = [37]; 
+  lista2= 20;
+  lista = 300; 
 
   constructor(private DataService$: DataService) { }
 
@@ -25,48 +25,47 @@ export class DashboardComponent implements OnInit {
     return this.DataService$.buscarDadosBD().subscribe(data => {
       this.analises = data;
       this.ok = data;
-      console.log(this.analises)
+      
     })
 
   }
 
   quantuser() {
     let cont = 0;
-    for (let user of this.analises.usuarios) {
-      if (user.nome != null) {
-        cont = cont + 1
+    /*console.log(this.analises)*/
+    for (let user of this.analises.usuarios){
+      if(user.usuario != null){
+        cont = cont+1
       }
     }
-    return cont;
+    return cont
   }
   quantComentario() {
     let contaC = 0;
-    for (let anal of this.analises.analises_geral) {
-      if (anal.tipo_analise == "Análise de Comentários") {
-        contaC = contaC + 1
-        this.qtAnalisecoment = contaC
+    
+    for (let analise of this.analises.analises_geral) {
+      /*console.log(analise.analiseComentarios)*/
+      if(analise.analiseComentarios != false){
+        contaC = contaC +1
       }
     }
-    return this.qtAnalisecoment;
+    return contaC  
+    
   }
+  
+    
   quantTexto() {
     let contT = 0;
-    for (let anal of this.analises.analises_geral) {
-      if (anal.tipo_analise == "Análise de Texto") {
-        contT = contT + 1
-        this.qtAnalisetexto = contT
-
-        this.lista2.push(contT)
+    /*console.log(this.analises.analises_geral)*/ 
+    for (let analise of this.analises.analises_geral){
+      console.log(analise.analiseLegenda)
+      if(analise.analiseLegenda != false){
+        contT = contT+1
+        
       }
     }
-    return this.qtAnalisetexto;
-  }
-
-  testao(){
-    for (let teste of this.analises.analise_foto){
-      return teste.tipoAnalise;
-    }
     
+    return contT 
   }
 
   public barChartOptions = {
@@ -80,7 +79,7 @@ export class DashboardComponent implements OnInit {
   public barChartLegend = true;
 
   public barChartData = [
-    { data: [this.lista, this.lista2], label: 'Series A' }
+    { data: [this.lista, this.lista2] }
   ];
 
 }
