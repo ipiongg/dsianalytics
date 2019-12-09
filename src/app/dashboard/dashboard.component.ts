8b973279cb1722508938809f2e881be5b4f73a26
André <andrefer483@gmail.com>
2019-12-09 04:43:47 -0300

    Adicionando o gráfico de Radar e outras coisas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewEncapsulation, ɵConsole } from '@angular/core';
 import { DataService } from '../data.service';
 import { map } from 'rxjs/operators';
-import { Chart } from 'chart.js';
+import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,32 +10,52 @@ import { Chart } from 'chart.js';
   styleUrls: ['./dashboard.component.css'],
   encapsulation: ViewEncapsulation.None
 })
+
 export class DashboardComponent implements OnInit {
+  //Gráfico de Pizza
   public barChartOptions
   public barChartLabens
   public barChartType
   public barChartLegend
   public barChartData
 
+  //Gráfico Radar
+  
+  
+  public felicidade = [0, 0, 0]; surpresa = [0, 0, 0]; medo = [0, 0, 0]; tristeza = [0, 0, 0]; raiva = [0, 0, 0]; nojo = [0, 0, 0]
+  public felicidadeF; surpresaF; medoF; tristezaF; raivaF; nojoF
+  public felicidadeL; surpresaL; medoL; tristezaL; raivaL; nojoL
+  public felicidadeC; surpresaC; medoC; tristezaC; raivaC; nojoC
+  
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+  };
+  public radarChartLabels: Label[] = ['Felicidade', 'Surpresa', 'Medo', 'Tristeza', 'Raiva', 'Nojo'];
+  public radarChartData: ChartDataSets[] = [
+    { data: [this.felicidadeF, this.surpresaF, this.medoF, this.tristezaF, this.raivaF, this.nojoF], label: 'Foto' },
+    { data: [this.felicidadeL, this.surpresaL, this.medoL, this.tristezaL, this.raivaL, this.nojoL], label: 'Legenda' },
+    { data: [this.felicidadeC, this.surpresaC, this.medoC, this.tristezaC, this.raivaC, this.nojoC], label: 'Comentários' },
+  ];
+
+  public radarChartType: ChartType = 'radar';
+
+  //Outras variáveis
   analises = null;
   ok = null;
   quantidade = 50;
-  qtAnalisetexto = null;
-  qtAnalisecoment = null;
+  qtAnaliseLegenda = null;
+  qtAnaliseComent = null;
   vermelho = [];
   azul = [];
   loading = false
 
-  public cont = 0;
-  public contC = 0;
-  public contT = 0;
-  public contF = 0;
+  public contU = 0; contC = 0; contL = 0; contF = 0;
   public qtd_analises_total = 0;
 
   constructor(public DataService$: DataService) { }
 
   ngOnInit() {
-  this.buscarDados();
+    this.buscarDados();
   }
 
   buscarDados() {
@@ -46,47 +67,139 @@ export class DashboardComponent implements OnInit {
 
       for (let user of this.ok.usuarios) {
         if (user.usuario != null) {
-          this.cont = this.cont + 1
+          this.contU = this.contU + 1
         }
+        console.log(this.radarChartData)
       }
 
-      //Análise de Textos
-      for (let analise of this.ok.analises_geral) {
-        if (analise.analiseLegenda != false) {
-          this.contT = this.contT + 1
-          //console.log(analise.analiseLegenda)
+      //Análise de Fotos
+      for (let foto of this.ok.analises_geral) {
+        if (foto.analiseFoto != false) {
+          this.contF = this.contF + 1
+          //console.log(foto.analiseFoto)
         }
+      } 
+
+      //Análise de Textos
+      for (let legenda of this.ok.analises_geral) {
+        if (legenda.analiseLegenda != false) {
+          this.contL = this.contL + 1
+          //console.log(texto.analiseLegenda.Resultado)
+        }
+        this.grafico_radar('Legenda', legenda.analiseLegenda.Resultado)
       }
 
       //Análise de Comentários
-      for (let analise of this.ok.analises_geral) {
-        if (analise.analiseComentarios != false) {
+      for (let comentario of this.ok.analises_geral) {
+        if (comentario.analiseComentarios != false) {
           this.contC = this.contC + 1
           //console.log(analise.analiseComentarios)
 
         }
       }
+
       this.vermelho.push(this.contC)
-      this.qtAnalisecoment = this.contC
+      this.qtAnaliseComent = this.contC
 
-      //Análise de Fotos
-      for (let analise of this.ok.analises_geral) {
-        if (analise.analiseFoto != false) {
-          this.contF = this.contF + 1
-          //console.log(analise.analiseFoto)
-        }
-      } 
-
-      this.qtAnalisetexto = this.contT
-      this.azul.push(this.contT)
+      this.qtAnaliseLegenda = this.contL
+      this.azul.push(this.contL)
       this.dados()
       this.loading = false
 
       //Retorna a quatidade de análises realizadas
-      this.qtd_analises_total = this.contT + this.contC + this.contF
+      this.qtd_analises_total = this.contL + this.contC + this.contF
     })
   }
 
+  grafico_radar(flc, sentimento){
+    if (flc == 'Foto') {
+      if (sentimento == 'Felicidade') {
+        
+      }
+      else if (sentimento == 'Surpresa'){
+
+      }
+      else if (sentimento == 'Medo'){
+  
+      }
+      else if (sentimento == 'Tristeza'){
+  
+      }
+      else if (sentimento == 'Raiva'){
+  
+      }
+      else if (sentimento == 'Nojo'){
+  
+      }
+
+    else if (flc == 'Legenda') {
+      if (sentimento == 'Felicidade') {
+        this.felicidade[1] += 1
+        this.felicidadeF = this.felicidade[1]*100/this.contL
+      }
+      else if (sentimento == 'Surpresa'){
+
+      }
+      else if (sentimento == 'Medo'){
+  
+      }
+      else if (sentimento == 'Tristeza'){
+  
+      }
+      else if (sentimento == 'Raiva'){
+  
+      }
+      else if (sentimento == 'Nojo'){
+  
+      }
+    }
+
+    else if (flc == 'Comentário') {
+      if (sentimento == 'Felicidade') {
+
+      }
+      else if (sentimento == 'Surpresa'){
+
+      }
+      else if (sentimento == 'Medo'){
+  
+      }
+      else if (sentimento == 'Tristeza'){
+  
+      }
+      else if (sentimento == 'Raiva'){
+  
+      }
+      else if (sentimento == 'Nojo'){
+  
+      }
+    }
+  }
+
+    /*
+    if (texto.analiseLegenda.Resultado == 'Felicidade'){
+
+    }
+    else if (texto.analiseLegenda.Resultado == 'Surpresa'){
+
+    }
+    else if (texto.analiseLegenda.Resultado == 'Medo'){
+
+    }
+    else if (texto.analiseLegenda.Resultado == 'Tristeza'){
+
+    }
+    else if (texto.analiseLegenda.Resultado == 'Raiva'){
+
+    }
+    else if (texto.analiseLegenda.Resultado == 'Nojo'){
+
+    }
+    */
+  }
+
+
+  //Gráfico de Pizza
   dados() {
     this.barChartOptions = {
       scaleShowVerticalLines: false,
@@ -103,5 +216,45 @@ export class DashboardComponent implements OnInit {
     ];
   }
 
+  //Gráfico Radar
+  /*dados2() {
+    this.barChartOptions = {
+      scaleShowVerticalLines: false,
+      responsive: true
 
+    };
+
+    this.barChartLabens = ['Analise Comentarios', 'Analise Texto'];
+    this.barChartType = 'radar';
+    this.barChartLegend = true;
+
+    this.barChartData = [
+      { data: [this.vermelho, this.azul], label: 'Analises' }
+    ];
+  }*/
+
+  /*
+  dados2() {
+    this.myRadarChart = new Chart(ctx, {
+      type: 'radar',
+      data: data,
+      options: options
+  });
+}
+  */
+
+  //Gráfico Radar
+  public chartClicked({ event, active }: {
+      event: MouseEvent, 
+      active: {}[] }): 
+        void {
+          console.log(event, active);
+        }
+
+  public chartHovered({ event, active }: { 
+    event: MouseEvent, 
+    active: {}[] }): 
+      void {
+        console.log(event, active);
+      }
 }
