88fe2831c922df69fd55aad9e3fe51a0a413f67d
athos vinicius <athos_47@hotmail.com>
2019-12-12 14:30:33 -0300

    Novo layout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, ViewEncapsulation, ɵConsole, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DataService } from '../data.service';
-import { map } from 'rxjs/operators';
-import { ChartDataSets, ChartType, RadialChartOptions, ChartOptions } from 'chart.js';
-import { Label, Color, BaseChartDirective } from 'ng2-charts';
+import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 import { AnalisesService } from 'src/app/analises.service';
-import { CanActivate } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -37,8 +35,9 @@ export class DashboardComponent implements OnInit {
 
   public felicidadeL = 0; medoL = 0; surpresaL = 0; tristezaL = 0;  raivaL = 0; nojoL = 0
   public felicidadeC = 0; medoC = 0; surpresaC = 0; tristezaC = 0;  raivaC = 0; nojoC = 0
+  public label = ['Felicidade', 'Medo', 'Surpresa', 'Tristeza', 'Raiva', 'Nojo'] 
 
-  public radarChartLabels: Label[] = ['Felicidade', 'Medo', 'Surpresa', 'Tristeza', 'Raiva', 'Nojo'];
+  public radarChartLabels: Label[] = this.label;
   public radarChartData: ChartDataSets[] = [
     //{ data: , label: 'Foto' },
     { data: this.legenda, label: 'Legenda' },
@@ -51,13 +50,9 @@ export class DashboardComponent implements OnInit {
   analises = null;
   ok = null;
   quantidade = 50;
-  qtAnaliseLegenda = null;
-  qtAnaliseComent = null;
   vermelho = [];
   azul = [];
   loading = false
-
-
 
   public contU = 0; contC = 0; contL = 0; contF = 0;
   public qtd_analises_total = 0;
@@ -81,14 +76,12 @@ export class DashboardComponent implements OnInit {
         if (user.usuario != null) {
           this.contU = this.contU + 1
         }
-        //console.log(this.radarChartData)
       }
 
       //Análise de Fotos
       for (let foto of this.ok.analises_geral) {
         if (foto.analiseFoto != false) {
           this.contF = this.contF + 1
-          //console.log(foto.analiseFoto)
         }
       }
 
@@ -96,156 +89,69 @@ export class DashboardComponent implements OnInit {
       for (let legenda of this.ok.analises_geral) {
         if (legenda.analiseLegenda != false) {
           this.contL = this.contL + 1
-          //console.log(texto.analiseLegenda.Resultado)
         }
         this.grafico_radar('Legenda', legenda.analiseLegenda.Resultado)
       }
+
       //Análise de Comentários
       for (let comentario of this.ok.analises_geral) {
         if (comentario.analiseComentarios != false) {
-          this.contC = this.contC + 1
-          console.log(comentario.analiseComentarios.Media)
+          this.contC += comentario.analiseComentarios.Lista_Frases.length
+
+          //Cálculo para o Gráfico de Radar
           for (let media of comentario.analiseComentarios.Media) {
-            if (media.Media_Felicidade > media.Media_Surpresa && media.Media_Felicidade > media.Media_Tristeza && 
-                media.Media_Felicidade > media.Media_Medo && media.Media_Felicidade > media.Media_Raiva && 
-                media.Media_Felicidade > media.Media_Nojo){
-
-              /*//console.log(0)
-              this.historico[0] += media.Media_Felicidade
-              //this.comentario[0] = this.historico[0]*100/this.porcentagem(media.Media_Felicidade)
-              this.porcentagem()
-              this.calculo()
-              //this.historico[0] = media.Media_Felicidade
-              //this.comentario[0] = (this.comentario[0]+media.Media_Felicidade)*100/teste*/
-
-              this.comentario[0] += 1
+            let valor = 0
+            let chave
+            let i = 0
+            for (let val of Object.values(media)) {
+              if (val > valor) {
+               /*valor = val*/
+                chave = Object.keys(media)[i]
+              }
+              i++
             }
 
-            else if(media.Media_Medo > media.Media_Felicidade && media.Media_Medo > media.Media_Tristeza && 
-              media.Media_Medo > media.Media_Surpresa && media.Media_Medo > media.Media_Raiva && 
-              media.Media_Medo > media.Media_Nojo){
-            //console.log(3)
-            /*this.historico[1] += media.Media_Medo
-            //this.comentario[1] = this.historico[1]*100/
-            this.porcentagem()
-            this.calculo()
-            //this.historico[3] += media.Media_Medo
-            //this.comentario[3] = media.Media_Medo*100/teste*/
-
-            this.comentario[1] += 1
-            }
-            else if(media.Media_Surpresa > media.Media_Felicidade && media.Media_Surpresa > media.Media_Tristeza && 
-                    media.Media_Surpresa > media.Media_Medo && media.Media_Surpresa > media.Media_Raiva && 
-                    media.Media_Surpresa > media.Media_Nojo){
-              /*//console.log(1)
-              this.historico[2] += media.Media_Surpresa
-              //this.comentario[2] += media.Media_Surpresa*100/
-              this.porcentagem()
-              this.calculo()*/
-              this.comentario[2] += 1
-            }
-            else if(media.Media_Tristeza > media.Media_Felicidade && media.Media_Tristeza > media.Media_Surpresa && 
-                    media.Media_Tristeza > media.Media_Medo && media.Media_Tristeza > media.Media_Raiva && 
-                    media.Media_Tristeza > media.Media_Nojo){
-              /*//console.log(2)
-              this.historico[3] += media.Media_Tristeza
-              //this.comentario[3] += media.Media_Tristeza*100/
-              this.porcentagem()
-              this.calculo()*/
-              this.comentario[3] += 1
-            }
-          
-            else if(media.Media_Raiva > media.Media_Felicidade && media.Media_Raiva > media.Media_Tristeza && 
-                    media.Media_Raiva > media.Media_Medo && media.Media_Raiva > media.Media_Surpresa && 
-                    media.Media_Raiva > media.Media_Nojo){
-              /*//console.log(4)
-              this.historico[4] += media.Media_Raiva
-              //this.comentario[4] += media.Media_Raiva*100/
-              this.porcentagem()
-              this.calculo()*/
-              this.comentario[4] += 1
-            }
-            else if(media.Media_Nojo > media.Media_Felicidade && media.Media_Nojo > media.Media_Tristeza && 
-                    media.Media_Nojo > media.Media_Medo && media.Media_Nojo > media.Media_Raiva && 
-                    media.Media_Nojo > media.Media_Surpresa){
-              /*//console.log(5)
-              this.historico[5] += media.Media_Nojo
-              //this.comentario[5] += media.Media_Nojo*100/
-              this.porcentagem()
-              this.calculo()*/
-              this.comentario[5] += 1
+            i = 0
+            for (let label of this.label) {
+              if (chave == 'Media_'+label) {
+                this.comentario[i] += 1
+              }
+              i++
             }
           }
         }
       }
 
+      //Alguns sets
       this.vermelho.push(this.contC)
-      this.qtAnaliseComent = this.contC
-
-      this.qtAnaliseLegenda = this.contL
       this.azul.push(this.contL)
       this.dados()
-      //this.dados2()
       this.loading = false
 
       //Retorna a quatidade de análises realizadas
       this.qtd_analises_total = this.contL + this.contC + this.contF
     })
   }
-
-  calculo(){
-    let i = 0 
-    while (i < 6) {
-      this.comentario[i] = this.historico[i]*100/this.porcentagem()
-      i++
-    }
-  }
-
-  porcentagem(){
-    console.log(this.historico)
-    console.log(this.comentario)
-    if ((this.comentario[0]+this.comentario[1]+this.comentario[2]+this.comentario[3]+this.comentario[4]+this.comentario[5]) == 0) {
-      console.log(this.comentario[0]+this.comentario[1]+this.comentario[2]+this.comentario[3]+this.comentario[4]+this.comentario[5])
-      return 100
-    }
-    else {
-      console.log(this.comentario[0]+this.comentario[1]+this.comentario[2]+this.comentario[3]+this.comentario[4]+this.comentario[5])
-      return this.comentario[0]+this.comentario[1]+this.comentario[2]+this.comentario[3]+this.comentario[4]+this.comentario[5]
-    }
-  }
   
   grafico_radar(flc, sentimento) {
     if (flc == 'Legenda') {
       if (sentimento == 'Felicidade') {
-        this.felicidadeL += 1
-        this.legenda[0] = this.felicidadeL*100/this.contL
-        //console.log(this.felicidadeL, this.contL, this.felicidadeL*100/this.contL)
-        console.log('felicidade')
-      }
-      else if (sentimento == 'Surpresa'){
-        this.surpresaL += 1
-        this.legenda[1] = this.surpresaL[1]*100/this.contL
-        console.log('surpresa')
-      }
-      else if (sentimento == 'Tristeza'){
-        this.tristezaL += 1
-        this.legenda[3] = this.tristezaL[1]*100/this.contL
-        console.log('tristeza')
+        this.legenda[0] += 1
       }
       else if (sentimento == 'Medo'){
-        this.raivaL += 1
-        this.legenda[2] = this.raivaL[1]*100/this.contL
-        console.log('medo')
+        this.legenda[1] += 1
+      }
+      else if (sentimento == 'Surpresa'){
+        this.legenda[2] += 1
+      }
+      else if (sentimento == 'Tristeza'){
+        this.legenda[3] += 1
       }
       else if (sentimento == 'Raiva'){
-        this.raivaL += 1
-        this.legenda[4] = this.raivaL[1]*100/this.contL
-        console.log('raiva')
+        this.legenda[4] += 1
       }
       else if (sentimento == 'Nojo'){
-        this.nojoL += 1
-        this.legenda[5] = this.nojoL[1]*100/this.contL
-        console.log('nojo')
+        this.legenda[5] += 1
       }
     }
   }
@@ -255,18 +161,15 @@ export class DashboardComponent implements OnInit {
     this.barChartOptions = {
       scaleShowVerticalLines: false,
       responsive: true
-
     };
 
-    this.barChartLabens = ['Comentário','Legenda'];
-    this.barChartType = 'pie';
+    this.barChartLabens = ['Legenda','Comentario'];
+    this.barChartType = 'horizontalBar';
     this.barChartLegend = true;
-    
 
     this.barChartData = [
-      
-      { data: [this.vermelho, this.azul], label:[ 'Comentarios']},
-     
+      { data: [this.contL], label:[ 'Legenda']},
+      {data: [this.contC], label:['Comentarios']},
     ];
   }
 }
